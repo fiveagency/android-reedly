09bbb42a37ebd96aca93626831f9591f3e0b7d82
David Gecek <davidgecek@Davids-MacBook-Pro.local>
2017-08-03 10:58:37 +0200

    adding test

---
+++ device/src/test/java/oxim/digital/reedly/device/job/JobsImplTest.java
@@ -0,0 +1,25 @@
+package oxim.digital.reedly.device.job;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JobsImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+
+    }
+
+    @Test
+    public void schedule() throws Exception {
+
+    }
+
+    @Test
+    public void cancel() throws Exception {
+
+    }
+}
