390dc64c79781deae84b7a5f6d82c9735220df3b
David <david@Davids-MacBook-Pro.local>
2017-09-22 16:48:50 +0200

    code style minor change

--- app/src/test/java/oxim/digital/reedly/AppTestData.java
+++ app/src/test/java/oxim/digital/reedly/AppTestData.java
@@ -4,7 +4,7 @@ public final class AppTestData {
 
     public static final int TEST_FEED_ID = 33;
     public static final int TEST_ARTICLE_ID = 44;
-    public static final String TEST_STRING = "bla";
+    public static final String TEST_STRING = "test_string";
     public static final String TEST_LINK = "http://com.com/";
     public static final long TEST_LONG_DATE = 1024l;
 
